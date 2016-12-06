6c833eeb694e0b8dc113875947853a26d1b9d046
Stephanie Campa <stephanie.campa@gmail.com>
2016-12-05 22:13:07 -0800

    Add files via upload

--- volunteeropportunities.js
+++ volunteeropportunities.js
@@ -56,8 +56,8 @@ var chosenTerm;
 
 
 
-//var chosenCat =  1; //This will give the category chosen in any number
-//var chosenNumber = 0; //This will grant the number 
+var chosenCat =  1; //This will give the category chosen in any number
+var chosenNumber = 0; //This will grant the number 
 //////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
@@ -178,7 +178,7 @@ function kitchenCall(){
     document.getElementById('kitchenOps').innerHTML = "";
     kitchenVisible = false; 
     button.value = "Kitchen: __";
-    //button.innerHTML = "Kitchen __";
+    button.innerHTML = "Kitchen __";
   }
   else if (kitchenVisible == false){
     ///THIS IS VERY SUBJECT TO APPRECIATION
@@ -194,20 +194,22 @@ function kitchenCall(){
 
         //THIS NUMELEMS VARIABLE IS VERY IMPORTANT
         numElems = snapshot.numChildren(); 
-        var kInfo = ""; 
+        var kInfo = "";
         //THIS SHOULD DEFINITELY BE EDITED FOR DATE AND OTHER VARIABLES THAT NEED TO BE SORTED!!!!!!!!!
+
+
         for (var i = 0; i < numElems; i++){
-            kInfo = kInfo +  '<div id = "text">' + databaseEvents[i].description + '</div>';
-            kInfo = kInfo + '<a href = "individualItemPage.html?id='+databaseEvents[i].id+'">';
+            kInfo = kInfo +  databaseEvents[i].description;
+            //kInfo = kInfo + '<a href = "individualItemPage.html?id='+databaseEvents[i].id+'">';
             kInfo = kInfo +  '<img src="' + databaseEvents[i].image + '"/>' ;
-            kInfo = kInfo + '</a>';
+            //kInfo = kInfo + '</a>';
         } 
         document.getElementById('kitchenOps').innerHTML = kInfo; 
       });
       //END DEFINITION
     kitchenVisible = true; 
     button.value = "Kitchen: Visible!";
-    //button.innerHTML = "Kitchrn: Visible!";
+    button.innerHTML = "Kitchrn: Visible!";
   }
 }
 
@@ -227,7 +229,7 @@ function officeCall(){
     document.getElementById('officeOps').innerHTML = "";
     officeVisible = false; 
     button.value = "Office: __";
-    //button.innerHTML = "Office: __!";
+    button.innerHTML = "Office: __!";
   }
   else if (officeVisible == false){
     ///THIS IS VERY SUBJECT TO APPRECIATION
@@ -247,17 +249,17 @@ function officeCall(){
         var kInfo = "";
         //THIS SHOULD DEFINITELY BE EDITED FOR DATE AND OTHER VARIABLES THAT NEED TO BE SORTED!!!!!!!!!
         for (var i = 0; i < numElems; i++){
-            kInfo = kInfo +  '<div id = "text">' + databaseEvents[i].description + '</div>';
-            kInfo = kInfo + '<a href = "individualItemPage.html?id='+databaseEvents[i].id+'">';
+            kInfo = kInfo +  databaseEvents[i].description;
+            //kInfo = kInfo + '<a href = "individualItemPage.html?id='+databaseEvents[i].id+'">';
             kInfo = kInfo +  '<img src="' + databaseEvents[i].image + '"/>' ;
-            kInfo = kInfo + '</a>';
+            //kInfo = kInfo + '</a>';
         } 
         document.getElementById('officeOps').innerHTML = kInfo; 
       });
       //END DEFINITION
     officeVisible = true; 
     button.value = "Office: Visible!";
-    //button.innerHTML = "Office: Visible!";
+    button.innerHTML = "Office: Visible!";
   }
 }
 
@@ -269,7 +271,7 @@ function corpCall(){
     document.getElementById('corpOps').innerHTML = "";
     corpVisible = false; 
     button.value = "Corp.: __";
-    //button.innerHTML = "Corp.: __";
+    button.innerHTML = "Corp.: __";
   }
   else if (corpVisible == false){
     ///THIS IS VERY SUBJECT TO APPRECIATION
@@ -288,17 +290,17 @@ function corpCall(){
         var kInfo = "";
         //THIS SHOULD DEFINITELY BE EDITED FOR DATE AND OTHER VARIABLES THAT NEED TO BE SORTED!!!!!!!!!
         for (var i = 0; i < numElems; i++){
-            kInfo = kInfo +  '<div id = "text">' + databaseEvents[i].description + '</div>';
-            kInfo = kInfo + '<a href = "individualItemPage.html?id='+databaseEvents[i].id+'">';
+            kInfo = kInfo +  databaseEvents[i].description;
+            //kInfo = kInfo + '<a href = "individualItemPage.html?id='+databaseEvents[i].id+'">';
             kInfo = kInfo +  '<img src="' + databaseEvents[i].image + '"/>' ;
-            kInfo = kInfo + '</a>';
+            //kInfo = kInfo + '</a>';
         } 
         document.getElementById('corpOps').innerHTML = kInfo; 
       });
       //END DEFINITION
     corpVisible = true; 
     button.value = "Corp.: Visible!";
-    //button.innerHTML = "Corp.: Visible!";
+    button.innerHTML = "Corp.: Visible!";
   }
 }
 
@@ -311,7 +313,7 @@ function schoolCall(){
     document.getElementById('schoolOps').innerHTML = "";
     schoolVisible = false; 
     button.value = "School: __";
-    //button.innerHTML = "School: __";
+    button.innerHTML = "School: __";
   }
   else if (schoolVisible == false){
     ///THIS IS VERY SUBJECT TO APPRECIATION
@@ -330,17 +332,17 @@ function schoolCall(){
         var kInfo = "";
         //THIS SHOULD DEFINITELY BE EDITED FOR DATE AND OTHER VARIABLES THAT NEED TO BE SORTED!!!!!!!!!
         for (var i = 0; i < numElems; i++){
-            kInfo = kInfo +  '<div id = "text">' + databaseEvents[i].description + '</div>';
-            kInfo = kInfo + '<a href = "individualItemPage.html?id='+databaseEvents[i].id+'">';
+            kInfo = kInfo +  databaseEvents[i].description;
+            //kInfo = kInfo + '<a href = "individualItemPage.html?id='+databaseEvents[i].id+'">';
             kInfo = kInfo +  '<img src="' + databaseEvents[i].image + '"/>' ;
-            kInfo = kInfo + '</a>';
+            //kInfo = kInfo + '</a>';
         } 
         document.getElementById('schoolOps').innerHTML = kInfo; 
       });
       //END DEFINITION
     schoolVisible = true; 
     button.value = "School: Visible!";
-    //button.innerHTML = "School: Visible!";
+    button.innerHTML = "School: Visible!";
   }
 }
 
